3bc684091dd38eab86d9fcc1c70d7b3533be1c9b
Andrey <andbel2@bk.ru>
2026-04-26 20:31:19 +0300

    feat: developes make_index_sequence

--- lib/stdd/apply_unpack.hpp
+++ lib/stdd/apply_unpack.hpp
@@ -8,6 +8,26 @@
 
 namespace stdd{
 
+
+
+template<size_t... Indexes>
+struct index_sequence{};
+
+template<size_t N, size_t... Indexes>
+struct make_index_sequence_impl{
+    using type = typename make_index_sequence_impl<N-1, N-1, Indexes...>::type;
+};
+
+template<size_t... Indexes>
+struct make_index_sequence_impl<0, Indexes...>{
+    using type = index_sequence<Indexes...>;
+};
+
+template<size_t N>
+using make_index_sequence = typename make_index_sequence_impl<N>::type;
+
+
+
 template <typename Arg>
 decltype(auto) Unpack(Arg&& arg) {
     if constexpr (is_future_v<std::remove_cvref_t<Arg>>) {
@@ -21,14 +41,14 @@ decltype(auto) Unpack(Arg&& arg) {
 
 template <typename F, typename... Args, std::size_t... Indxs>
 decltype(auto) apply_impl(F&& func, stdd::Tuple<Args...>& tuple,
-                          std::index_sequence<Indxs...> indxs) {
+                          index_sequence<Indxs...> indxs) {
     return stdd::forward<F>(func)(Unpack(tuple.template get<Indxs>())...);
 }
 
 template <typename F, typename... Args>
 decltype(auto) ApplyAndUnpack(F&& func, stdd::Tuple<Args...>& tuple) {
     return apply_impl(stdd::forward<F>(func), tuple,
-                      std::make_index_sequence<sizeof...(Args)>());
+                      make_index_sequence<sizeof...(Args)>());
 }
 
 }

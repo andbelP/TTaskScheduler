ea486d9ec371cd4040f8dadab22b7636459e6f2a
Andrey <andbel2@bk.ru>
2026-04-29 14:46:45 +0300

    feat: now using TTaskScheduler in RecommendApplication

--- src/RecommendApplication/application/application.cpp
+++ src/RecommendApplication/application/application.cpp
@@ -1,35 +1,90 @@
-#include <expected>
-#include <string>
-#include <iostream>
-
 #include "application.hpp"
+
+#include <expected>
+#include <iostream>
+#include <string>
+
+#include "TTaskScheduler/TTaskScheduler.hpp"
 #include "errors_handling/check_expected.hpp"
 
-std::expected<void, std::string> Application::Run(){
-    auto geoposition = two_ip_client_.Get();
-    PROPAGATE_EXPECTED(geoposition);
+std::expected<void, std::string> Application::Run() {
+    TTaskScheduler scheduler;
 
-    auto weather = openweathermap_client_.Get(geoposition.value());
-    PROPAGATE_EXPECTED(weather);
+    auto geoposition_task =
+        scheduler.add([this]() -> std::expected<Geoposition, std::string> {
+            auto geoposition = two_ip_client_.Get();
+            PROPAGATE_EXPECTED(geoposition);
 
-    auto interesting_places = overpass_client_.Get(geoposition.value(), weather.value(), cnt_of_places_);
-    PROPAGATE_EXPECTED(interesting_places);
+            return geoposition.value();
+        });
 
-    for (const auto& place : interesting_places.value()) {
+    auto weather_task = geoposition_task.apply(
+        [this](std::expected<Geoposition, std::string>& geoposition_expected)
+            -> std::expected<Weather, std::string> {
+            PROPAGATE_EXPECTED(geoposition_expected);
 
-        auto distance = distance_client_.Get(geoposition.value(), place.geoposition);
-        PROPAGATE_EXPECTED(distance);
+            auto weather = openweathermap_client_.Get(geoposition_expected.value());
+            PROPAGATE_EXPECTED(weather);
 
-        std::cout << "\n-----------------------------------------\n";
-        std::cout << "Name: " << place.name << "\n";
-        std::cout << "Website: " << place.website << "\n";
-        std::cout << "Email: " << place.email << "\n";
-        std::cout << "Info: " << place.info << "\n";
-        std::cout << "Description: " << place.description << "\n";
-        std::cout << "Location: (" << place.geoposition.latitude << ", " << place.geoposition.longitude << ")\n";
-        std::cout << "Distance: " << distance.value().distance << " meters\n";
-        std::cout << "-----------------------------------------\n";
-    }
+            return weather.value();
+        });
+
+    auto interesting_places_task = scheduler.add(
+        [this](std::expected<Weather, std::string>& weather_expected,
+               std::expected<Geoposition, std::string>& geoposition_expected,
+               int cnt_of_places_)
+            -> std::expected<std::vector<InterestingPlace>, std::string> {
+            PROPAGATE_EXPECTED(weather_expected);
+            PROPAGATE_EXPECTED(geoposition_expected);
+
+            auto& weather = weather_expected.value();
+            auto& geoposition = geoposition_expected.value();
+
+            auto interesting_places =
+                overpass_client_.Get(geoposition,
+                                     weather, cnt_of_places_);
+            PROPAGATE_EXPECTED(interesting_places);
+
+            return interesting_places.value();
+        },
+        weather_task.getFutureResult<std::expected<Weather, std::string>&>(),
+        geoposition_task.getFutureResult<std::expected<Geoposition, std::string>&>(),
+        cnt_of_places_);
+
+
+    auto print_task = scheduler.add(
+        [this](std::expected<std::vector<InterestingPlace>, std::string>& interesting_places_expected,
+               std::expected<Geoposition, std::string>& geoposition_expected)
+            -> std::expected<void, std::string> {
+
+            PROPAGATE_EXPECTED(interesting_places_expected);
+            PROPAGATE_EXPECTED(geoposition_expected);
+
+            auto& interesting_places = interesting_places_expected.value();
+            auto& geoposition = geoposition_expected.value();
+
+            for (const auto& place : interesting_places) {
+
+                auto distance = distance_client_.Get(geoposition,place.geoposition); 
+                PROPAGATE_EXPECTED(distance);
+
+                std::cout << "\n-----------------------------------------\n";
+                std::cout << "Name: " << place.name << "\n";
+                std::cout << "Website: " << place.website << "\n";
+                std::cout << "Email: " << place.email << "\n";
+                std::cout << "Info: " << place.info << "\n";
+                std::cout << "Description: " << place.description << "\n";
+                std::cout << "Location: (" << place.geoposition.latitude << ", " << place.geoposition.longitude << ")\n"; 
+                std::cout << "Distance: " << distance.value().distance << " meters\n"; 
+                std::cout << "-----------------------------------------\n";
+            }
+
+            return {};
+
+        }, interesting_places_task.getFutureResult<std::expected<std::vector<InterestingPlace>, std::string>&>(), geoposition_task.getFutureResult<std::expected<Geoposition, std::string>&>());
+
+
+    scheduler.executeAll();
 
     return {};
 }
--- src/RecommendApplication/main.cpp
+++ src/RecommendApplication/main.cpp
@@ -1,34 +1,48 @@
-#include <iostream>
+    #include <iostream>
 
-#include "application/application.hpp"
-#include "api_clients/geo_by_ip_client/twoip_client.hpp"
-#include "api_clients/weather_client/openweathermap_client.hpp"
-#include "api_clients/maps_client/overpass_client.hpp"
-#include "api_clients/distance_client/OSRM_client.hpp"
-#include "api_clients/http_client/cpr_http_client.hpp"
+    #include "application/application.hpp"
+    #include "api_clients/geo_by_ip_client/twoip_client.hpp"
+    #include "api_clients/weather_client/openweathermap_client.hpp"
+    #include "api_clients/maps_client/overpass_client.hpp"
+    #include "api_clients/distance_client/OSRM_client.hpp"
+    #include "api_clients/http_client/cpr_http_client.hpp"
 
 
-int main(int argc, char** argv){
-    int N = 10;
-    if(argc == 2){
-        N = std::stoi(std::string(argv[1]));
+    int main(int argc, char** argv){
+        int N = 10;
+        if(argc == 2){
+            N = std::stoi(std::string(argv[1]));
+        }
+
+        auto raw_2ip_api_key = std::getenv("IP2_API_KEY");
+
+        if (raw_2ip_api_key == nullptr || std::string(raw_2ip_api_key).empty()) {
+            std::cerr << "Cannot read 2IP api key";
+            return 1;
+        }
+
+        auto raw_openweathermap_api_key = std::getenv("OPENWEATHERMAP_API_KEY");
+
+        if (raw_openweathermap_api_key == nullptr || std::string(raw_openweathermap_api_key).empty()) {
+            std::cerr << "Cannot read OpenWeatherMap api key";
+            return 1;
+        }
+
+        CprHttpClient http_client;
+
+        TwoIpClient two_ip_client(raw_2ip_api_key, http_client);
+
+        OpenWeatherMapClient openweathermap_client(raw_openweathermap_api_key, http_client);
+
+        OverpassClient overpass_client(http_client);
+
+        OSRMClient distance_client(http_client);
+
+        Application application(N, two_ip_client, openweathermap_client, overpass_client, distance_client);
+
+        auto result = application.Run();
+        if(!result){
+            std::cerr << "Error: " << result.error() << "\n";
+            return 1;
+        }
     }
-
-    CprHttpClient http_client;
-
-    TwoIpClient two_ip_client("wvov15ahnquoi9lv", http_client);
-
-    OpenWeatherMapClient openweathermap_client("e7ecc1aaf49378d4b45e6b3f4c2f8c92", http_client);
-
-    OverpassClient overpass_client(http_client);
-
-    OSRMClient distance_client(http_client);
-
-    Application application(N, two_ip_client, openweathermap_client, overpass_client, distance_client);
-
-    auto result = application.Run();
-    if(!result){
-        std::cerr << "Error: " << result.error() << "\n";
-        return 1;
-    }
-}
